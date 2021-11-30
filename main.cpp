fc5eb46ef0ae1e1d516a46902e7f6780afaf752a
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-11-30 09:17:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char input;
+    cin >> input;
+    if (conversione(input)== false){
+        cout << "errore" << endl;
+    }else{
+        cout << input << endl;
+    }
+    return 0;
 }
