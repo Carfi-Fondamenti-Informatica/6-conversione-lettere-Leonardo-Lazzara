69f3811152bce2c126f92068bebe3257502f6537
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-11-30 09:16:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+bool conversione(char &a) {
+        if (a >= 'A' && a <= 'Z') {
+            a = a + 32;
+            return true;
+        } else if (a >= 'a' && a <= 'z') {
+            a= a - 32;
+            return true;
+        }
+        return false;
+}
